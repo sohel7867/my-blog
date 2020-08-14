f665e6381405504578a05ba7f68b4fde53ed7a3e
sohel <sohelmira28@gmail.com>
2020-08-14 09:35:48 +0530

    setting.py

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'pydjblog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-DATABASES['default'] = dj_database_url.config(default='postgres://...')
+'''
+DATABASES['default'] = dj_database_url.config(default='postgres://...')'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
